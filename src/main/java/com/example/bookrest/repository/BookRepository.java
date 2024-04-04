785f5ad8291f611d3b2cda7a079daa3a14b20a8d
Alexey Yurchenko <alexeyyurchenko.java@gmail.com>
2024-04-04 21:34:58 +0700

    next commit

--- src/main/java/com/example/bookrest/repository/BookRepository.java
+++ src/main/java/com/example/bookrest/repository/BookRepository.java
@@ -4,6 +4,15 @@ import com.example.bookrest.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    List<Book> findByCategoryId(Long CategoryId);
+
+    Optional<Book> findByName (String name);
+
+    Optional<Book> findByAuthor(String author);
 }
--- src/main/java/com/example/bookrest/service/BookService.java
+++ src/main/java/com/example/bookrest/service/BookService.java
@@ -1,16 +1,62 @@
 package com.example.bookrest.service;
 
+import com.example.bookrest.entity.Book;
+import com.example.bookrest.entity.Category;
+import com.example.bookrest.exception.EntityNotFoundException;
 import com.example.bookrest.repository.BookRepository;
 import com.example.bookrest.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
     private final CategoryRepository categoryRepository;
+
+    public List<Book> findAll() {
+        log.debug("BookService -> findAll");
+        return bookRepository.findAll();
+    }
+    public Book findById(Long id) {
+        log.debug("BookService -> findById id = {}", id);
+        return bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(MessageFormat
+                .format("Book with ID {0} not found",id)));
+    }
+    public Category findByIdCategory(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(MessageFormat
+                .format("Category with ID {0} not found",id)));
+    }
+    public List<Book> findByCategoryName(String name) {
+        Optional<Category> category = categoryRepository.findByName(name);
+        if (category.isPresent()) {
+            return bookRepository.findByCategoryId(category.get().getId());
+        } else {
+            throw new EntityNotFoundException(MessageFormat.format("Category with name {0} not found",name));
+        }
+    }
+    public Book findByName(String name) {
+        return bookRepository.findByName(name).orElseThrow(()->
+                new EntityNotFoundException(MessageFormat.format("Book with name {0} not found",name)));
+    }
+    public Book findByAuthor(String author) {
+        return bookRepository.findByAuthor(author).orElseThrow(()->
+                new EntityNotFoundException(MessageFormat.format("Book with author {0} not found",author)));
+    }
+
+    public Book save(Book book) {
+        Category category = findByIdCategory(book.getId());
+        category.addBook(book);
+        return bookRepository.save(book);
+    }
+
 
 
 
